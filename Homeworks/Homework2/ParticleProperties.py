6cfd9ac3365e23310ec1bc44a6e6446e10d7e7f7
travismatlock <travismatlock@arizona.edu>
2023-01-25 15:05:01 -0700

    Adjusted ParticleInfo to use particle type

--- Homeworks/Homework2/ParticleProperties.py
+++ Homeworks/Homework2/ParticleProperties.py
@@ -12,29 +12,26 @@ from ReadFile import Read
 
 # This function takes a file name, particle type, and particle number (starting
 # from 1). The file name is a string, particularly 'MW_nnn.txt'. The particle
-# type is not used yet. The particle number is an integer and starts with the
-# first particle. Index corrections are built into the code. It returns the
-# magnitude of the distance from the origin in kpc, the magnitude of the
+# type is an integer 1, 2, or 3. The particle number is an integer and starts 
+# with the first particle. Index corrections are built in the code. It returns 
+# the magnitude of the distance from the origin in kpc, the magnitude of the
 # velocity in km/s, and the mass in solar masses of the specified particle.
-def ParticleInfo(filename, type, num):
+def ParticleInfo(filename, ptype, num):
     # Grab variables from ReadFile. total and time are not yet used.
     time, total, data = Read(filename)
+    # Create a filter that selects particles only with the specified type.
+    temp = np.where(data['type'] == ptype)
+    # Create an array based on data with only particles of the same type
+    type_sorted = data[temp]    
     # Calculate the magnitude of the distance vector
-    distance = math.hypot(data['x'][num-1], data['y'][num-1], data['z'][num-1])
+    distance = math.hypot(type_sorted['x'][num-1], type_sorted['y'][num-1], type_sorted['z'][num-1])
     # Assign units and round to three decimal places
     distance = np.around(distance * u.kpc, 3)
     # Calculate the magnitude of the velocity vector
-    speed = math.hypot(data['vx'][num-1], data['vy'][num-1], data['vz'][num-1])
+    speed = math.hypot(type_sorted['vx'][num-1], type_sorted['vy'][num-1], type_sorted['vz'][num-1])
     # Assign units and round to three decimal paces
     speed = np.around(speed * u.km / u.s, 3)
     # Extract the mass of and assign units to the particle
-    mass = data['m'][num-1] * u.solMass
+    mass = type_sorted['m'][num-1] * u.solMass
     # Return finished distance, speed, and mass
     return distance, speed, mass
-
-
-distance, speed, mass = ParticleInfo('MW_000.txt', 'NA', 100)
-print("3D distance: "+str(distance))
-print("3D velocity: "+str(speed))
-print("Mass: "+str(mass))
-print("3D distance in light years: "+str(distance.to(u.lyr)))
